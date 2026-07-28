a2bbf20fd896725e45b92bad5898e98143957f5d
ParameswarSurampudi <parameswarsurampudi@gmail.com>
2026-07-28 13:43:10 +0530

    Project v3.0

--- src/main/java/com/klef/soa/service/DoctorService.java
+++ src/main/java/com/klef/soa/service/DoctorService.java
@@ -7,7 +7,7 @@ import com.klef.soa.entity.Doctor;
 public interface DoctorService {
 	
 	 Doctor addDoctor(Doctor d);
-	 Doctor updatDoctor(Doctor d);
+	 Doctor updateDoctor(Doctor d);
 	 List<Doctor> displayAllDoctors();
 	 Doctor displayDoctorById(Long id);
 	 String deleteDoctorById(Long id);
